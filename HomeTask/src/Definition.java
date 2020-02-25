db03129a56222f35aebf4e0e72ec2e6bf0e2d3b7
Vasily Kulikov <zel.slonik17@gmail.com>
2020-02-25 12:03:21 +0300

    Код второго урока.

--- HomeTask/src/Definition.java
+++ HomeTask/src/Definition.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class Definition {
+    public static void printArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int [][] a = new int[10][12];
+        printArr(a);
+        int[][] table = new int[3][];
+        table[0] = new int[1];
+        table[1] = new int[5];
+        table[2] = new int[3];
+        System.out.print(Arrays.deepToString(table));
+    }
 }
